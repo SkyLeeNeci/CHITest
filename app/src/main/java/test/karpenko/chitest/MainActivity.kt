8803849742c07685c67312d4fa89673cc0af2364
VladyslavKarpenko <92163391+SkyLeeNeci@users.noreply.github.com>
2022-09-21 15:14:29 +0300

    Solutions for level 1 (Middle)

--- app/src/main/java/test/karpenko/chitest/MainActivity.kt
+++ app/src/main/java/test/karpenko/chitest/MainActivity.kt
@@ -2,10 +2,41 @@ package test.karpenko.chitest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import test.karpenko.chitest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var counter = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.increaseCounterBtn.setOnClickListener {
+            counter += 1
+            binding.counterTV.text = counter.toString()
+        }
+
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+
+        outState.run {
+            putInt("COUNT", counter)
+        }
+        super.onSaveInstanceState(outState)
+    }
+
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        counter = savedInstanceState.getInt("COUNT")
+        binding.counterTV.text = counter.toString()
+
+    }
+
 }
